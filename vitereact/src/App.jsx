fcd7acb1606bfb2d96838fcc95d26da64801ba53
Harshita Saini <harshitasaini283@gmail.com>
2026-03-12 18:24:18 +0530

    understanding of files

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+
+function App() {
+  {/* jab bhi hum component banate hai to uska naam capital letter se start hona chahiye and file name bhi capital letter se start hona chahiye */}
+
+  return (
+    <>
+      <h1>
+        <i>
+          harshita
+        </i>
+      </h1>
+      <h1><i>boom
+        </i></h1>
+        
+    </>
+  )
+}
+
+export default App
