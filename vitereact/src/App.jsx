2095bb757e74d1afff88d6c586a8549f364655b5
Harshita Saini <harshitasaini283@gmail.com>
2026-03-13 16:02:37 +0530

    counter project

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -5,16 +5,12 @@ import './App.css'
 function App() {
   {/* jab bhi hum component banate hai to uska naam capital letter se start hona chahiye and file name bhi capital letter se start hona chahiye */}
 
+  const username="chai or code"
+
   return (
     <>
-      <h1>
-        <i>
-          harshita
-        </i>
-      </h1>
-      <h1><i>boom
-        </i></h1>
-        
+      <h1>harshita{username}</h1>
+      /*{username} ek evaluate expression hai and ya username variable ki value ko render karega and {}ko use karke hum us variable ki value ko render kar sakte hai*/
     </>
   )
 }
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import React from "react"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/*const reactElement ={ -- ye react syntax yahan directly yahan run nhi hoga isliye humne custom react banaya hai jisme humne react element ko object ke form me likha hai
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    Children:'click me to visit google'
+}*/
+
+const anotherElement =(
+  <a href="https://google.com" target="_blank">click me to visit google</a>
+)//ye react element hai jise humne jsx syntax me likha hai isko hum directly render kar sakte hai react ke andar
+
+// react me bhi hum element create kar sakte hai react.createElement() method ke through jisme hum type , props aur children ko pass karte hai
+const anotherElement2 = React.createElement('a',{href:'https://google.com',target:'_blank'},'click me to visit google') /* isme humne type , props aur children ko pass kar diya hai isse bhi hum directly render kar sakte hai react ke andar */
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <App/>
+  
 )
